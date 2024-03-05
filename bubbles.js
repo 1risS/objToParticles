1cdf52f9145ccdeb2e2dc704ab9a0e935f0ed218
Iris Saladino <iris@genosha.com.ar>
2024-03-05 17:18:59 -0300

    avances

---
+++ bubbles.js
@@ -0,0 +1,65 @@
+import * as THREE from 'three';
+
+import { pointMaterial } from "./main";
+
+let particleCount, particles, positions;
+let _showBubbles = false;
+
+export function initBubbles(scene) {
+    particleCount = 2000;
+    particles = new THREE.BufferGeometry();
+    positions = new Float32Array(particleCount * 3);
+
+    for (let i = 0; i < particleCount * 3; i += 3) {
+        const x = Math.random() * 2 - 1;
+        const y = Math.random() * 0.9 + -1.5;
+        const z = Math.random() * 2 - 1;
+
+        positions[i] = x;
+        positions[i + 1] = y;
+        positions[i + 2] = z;
+    }
+
+    particles.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+
+    const particleSystem = new THREE.Points(particles, pointMaterial);
+    scene.add(particleSystem);
+}
+
+export function showBubbles() {
+    _showBubbles = true
+}
+
+
+export function animateBubbles() {
+    if (_showBubbles) {
+
+        if (particles) {
+
+            const positionAttribute = particles.getAttribute('position');
+            const array = positionAttribute.array;
+            let speed = 2;
+            for (let i = 0; i < particleCount * 3; i += 3) {
+                array[i + 1] += Math.random() * 0.01 * speed;
+                array[i] += Math.sin(
+                    array[i + 1]
+                    // * Math.random() 
+                    + i
+                ) * 0.01;
+
+                // Reset particles that reach top
+                // if (array[i + 1] > 1) {
+                //     array[i + 1] = -1;
+                //     array[i] = Math.random() * 2 - 1;
+                //     array[i + 2] = Math.random() * 2 - 1;
+                // }
+            }
+
+            positionAttribute.needsUpdate = true;
+        }
+
+        // renderer.render(scene, camera);
+
+        // requestAnimationFrame(animateParticles);
+    }
+}
--- main.js
+++ main.js
@@ -5,37 +5,56 @@ import { WebGLRenderer } from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-let camera, scene, renderer;
+import { initBubbles, animateBubbles, showBubbles } from './bubbles';
+
+export let camera;
+export let scene;
+export let renderer;
 
 let controls;
 
+//let bubbleTexture = new THREE.TextureLoader().load('imgs/bubble.png')
+
+// burbujas
+export const pointMaterial = new THREE.PointsMaterial({
+  color: 'white',
+  size: 0.015,
+  //map: bubbleTexture,
+  map: createCircleTexture(),
+  transparent: true,
+  alphaTest: 0.5,
+  opacity: 0.75,
+  blending: THREE.AdditiveBlending
+});
+
 init();
+
+
 
 function init() {
   // camara
   camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.set(1, 1, 1);
+  camera.position.set(0, 0, 3);
   //escena
   scene = new THREE.Scene();
   //loader
   const loader = new GLTFLoader();
   loader.load(
-    'meshes/cara_01.glb', function (gltf) {
+    'meshes/cara_02.glb', function (gltf) {
 
 
       const object = gltf.scene;
 
-      object.position.set(0, 0, 0);
-
-      object.visible = false;
-      scene.add(object);
+      // object.position.set(0, 0, 0);
+      // scene.add(object);
+      // object.visible = false;
 
       let pts = [];
       let v3 = new THREE.Vector3();
       object.traverse(child => {
         if (child.isMesh) {
           let pos = child.geometry.attributes.position;
-          for (let i = 0; i < pos.count; i++) {
+          for (let i = 0; i < pos.count * 3; i++) {
             v3.fromBufferAttribute(pos, i);
             pts.push(v3.clone());
           }
@@ -44,15 +63,17 @@ function init() {
 
       let g = new THREE.BufferGeometry().setFromPoints(pts);
       g.center();
-      let m = new THREE.PointsMaterial({ color: "aqua", size: 0.025 });
 
-      let p = new THREE.Points(g, m);
+      let p = new THREE.Points(g, pointMaterial);
 
       p.scale.setScalar(10)
+      p.rotation.x = Math.PI * 0.5
 
       scene.add(p)
 
     });
+
+  initBubbles(scene)
 
   //luz
   const light = new THREE.AmbientLight(0x404040); // soft white light
@@ -76,6 +97,28 @@ function init() {
   controls.update();
 }
 
+export function createCircleTexture() {
+  const size = 64;
+  const canvas = document.createElement('canvas');
+  canvas.width = size;
+  canvas.height = size;
+
+  const context = canvas.getContext('2d');
+  const centerX = size / 2;
+  const centerY = size / 2;
+  const radius = size / 2;
+
+  context.beginPath();
+  context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+  context.fillStyle = "#FFF"; // Color blanco
+  context.fill();
+
+  const texture = new THREE.Texture(canvas);
+  texture.needsUpdate = true;
+
+  return texture;
+}
+
 //responsive canvas
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -85,5 +128,11 @@ function onWindowResize() {
 
 function animate() {
   controls.update();
+  animateBubbles(scene);
   renderer.render(scene, camera);
 }
+
+
+document.addEventListener('click', function () {
+  showBubbles()
+})
