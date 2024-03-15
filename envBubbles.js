8832e034ce73ec3373b1959aa9f83c64d8f70117
Iris Saladino <iris.r.saladino@gmail.com>
2024-03-15 09:17:17 -0300

    accel

--- envBubbles.js
+++ envBubbles.js
@@ -3,6 +3,11 @@ import * as THREE from 'three';
 let particleCount, particles, positions;
 let _showBubbles = true;
 
+const baseAccel = 0.0009;
+const accelStep = 0.00005;
+const maxAccel = 0.02;
+const accel = [];
+
 export function initEnvBubbles(scene, texture) {
     const material = new THREE.PointsMaterial({
         color: 'white',
@@ -12,13 +17,13 @@ export function initEnvBubbles(scene, texture) {
         opacity: 1.0,
     });
 
-    particleCount = 2000;
+    particleCount = 100;
     particles = new THREE.BufferGeometry();
     positions = new Float32Array(particleCount * 3);
 
     for (let i = 0; i < particleCount * 3; i += 3) {
         const x = Math.random() * 2 - 1;
-        const y = Math.random() * 0.9 + -1.5;
+        const y = Math.random() * 4 + -5.5;
         const z = Math.random() * 8 - 4;
 
         positions[i] = x;
@@ -27,6 +32,10 @@ export function initEnvBubbles(scene, texture) {
     }
 
     particles.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+
+    for (let i = 0; i < particleCount; i += 1) {
+        accel.push(baseAccel);
+    }
 
     const particleSystem = new THREE.Points(particles, material);
     scene.add(particleSystem);
@@ -43,23 +52,25 @@ export function animateEnvBubbles() {
 
             const positionAttribute = particles.getAttribute('position');
             const array = positionAttribute.array;
-            let speed = 0.000009;
-            let acceleration = 0.000005;
-            for (let i = 0; i < particleCount * 3; i += 3) {
-                array[i + 1] += Math.random() * speed;
+
+            for (let i = 0, j = 0; i < particleCount * 3; i += 3, j += 1) {
+                array[i + 1] += Math.random() * accel[j];
 
                 // Reset particles that reach top
                 if (array[i + 1] > 3) {
-                    acceleration = 0.0000005;
+                    const x = Math.random() * 2 - 1;
+                    const y = Math.random() * 3 + -3.5;
+                    const z = Math.random() * 8 - 4;
 
-                    array[i + 1] = -2;
-                    array[i] = Math.random() * 2 - 1;
-                    array[i + 2] = Math.random() * 2 - 1;
-                    speed = 0.001;
+                    array[i] = x;
+                    array[i + 1] = y;
+                    array[i + 2] = z;
                 }
 
-                // Increase speed for next iteration
-                speed += acceleration;
+                // Increase acceleration (unless reached the maximum)
+                if (accel[j] < maxAccel) {
+                    accel[j] += accelStep;
+                }
             }
 
             positionAttribute.needsUpdate = true;
